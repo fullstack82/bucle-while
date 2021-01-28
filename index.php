3efea91b4d02ac6b5b92738b28450e19308fbc48
fullstack82 <fullstack8210@gmail.com>
2021-01-28 23:56:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/* LO QUE HACE UN BUCLE ES EJECUTAR UN BLOQUE DE INSTRUCCIONES TANTAS VECES COMO SEA NECESARIO 
+
+#Bucle while
+
+#Estructura de control que itera o repite la ejecución de una serie de instrucciones tantas veces como sea necesario, en base a una condición que el bucle lleva.
+
+*/
